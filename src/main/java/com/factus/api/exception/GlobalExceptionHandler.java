01a7dd2e07c99eda315c9d3e09f1cecfa7850b30
RojasS3015 <sebasrush0521@gmail.com>
2026-03-13 12:26:19 -0300

    feat(exception): implement GlobalExceptionHandler to unify Factus API errors

---
+++ src/main/java/com/factus/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.factus.api.exception;
+
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.factus.api.dtos.response.ErrorResponse;
+
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @ExceptionHandler(FactusClientException.class)
+    public ResponseEntity<ErrorResponse> handleFactusClientException(FactusClientException ex){
+        List<String> detalles = new ArrayList<>();
+        String errorRaw = ex.getMessage();
+
+        try {
+            JsonNode root = objectMapper.readTree(errorRaw);
+            JsonNode errorsNode = root.path("Data").path("errors");
+
+            // Si el nodo "errors" es un objeto (como el 422: campo -> [error])
+            if (errorsNode.isObject()) {
+                errorsNode.fields().forEachRemaining(entry -> {
+                    entry.getValue().forEach(node -> detalles.add(node.asText()));
+                });
+                
+            
+            }// Si el nodo "errors" es una lista (como el 409)
+            else if (errorsNode.isArray()) {
+                errorsNode.forEach(node -> detalles.add(node.path("message").asText()));
+
+            }else{
+                detalles.add(root.path("message").asText("Error desconocido en Factus"));
+            }
+        } catch (Exception e) {
+            detalles.add("No se pudo procesar la respuesta de error original: " + errorRaw);
+        }
+        ErrorResponse error = new ErrorResponse(
+            "FACTUS_CLIENT_ERROR",
+            "La API de Factus rechazo la solicitud",
+            detalles,
+            LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+    }
+}
