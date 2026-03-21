38f18142460a40a20bb579d6cd08e1d855e01b92
RojasS3015 <sebasrush0521@gmail.com>
2026-03-20 23:05:37 -0300

    feat(config): habilitar CORS en WebFlux para conexión con el frontend

--- src/main/java/com/factus/api/infrastructure/config/WebFluxConfig.java
+++ src/main/java/com/factus/api/infrastructure/config/WebFluxConfig.java
@@ -1,6 +1,7 @@
 package com.factus.api.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
@@ -12,5 +13,13 @@ public class WebFluxConfig implements WebFluxConfigurer {
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/")
                 .resourceChain(false);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir en todos los endpoints
+                .allowedOrigins("*") // En producción, cambia "*" por la URL de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
